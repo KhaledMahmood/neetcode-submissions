cbecd7e0d78b8f6fc060feb22e7b9c0fd1a95048
Khaled Mahmood <32155171+KhaledMahmood@users.noreply.github.com>
2026-09-09 07:47:14 +0600

    Add: pow-x-n - submission-5

---
+++ Data Structures & Algorithms/pow-x-n/submission-5.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+
+        if(x == 0.0L) return 0.0L;
+
+        double res = getPow(x, abs(n));
+        
+        return n >= 0 ? res : 1.000/res;
+        
+    }
+
+    double getPow(double x, int n) {
+
+        if(n == 0) {
+            return 1;
+        }
+
+        double res = getPow(x, n/2);
+        return res * res * ((n % 2) ? x : 1);
+    }
+
+};
